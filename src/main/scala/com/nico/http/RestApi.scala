a53549ff7e7a36421a22dc8c34f7449eccaafde3
Nicolas A Perez <anicolaspp@gmail.com>
2016-07-06 13:20:41 -0400

    The RestApi is now part of the cluster

--- src/main/scala/com/nico/http/RestApi.scala
+++ src/main/scala/com/nico/http/RestApi.scala
@@ -12,18 +12,22 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
 import com.nico.persistence.Transaction
+import com.typesafe.config.ConfigFactory
 
 import scala.io.StdIn
 
 object RestApi extends App with ApiTransactionResources {
-  //override val transactionPublisher: TransactionPublisher = _
 
-  implicit val system = ActorSystem()
+  val configuration = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=9070")
+    .withFallback(ConfigFactory.parseString("akka.cluster.roles=[api]"))
+    .withFallback(ConfigFactory.load())
+
+  implicit val system = ActorSystem("TransactionCluster", configuration)
   implicit val materializer = ActorMaterializer()
 
   val api = transactionRoutes
 
-  val binding = Http().bindAndHandle(api, "localhost", 9070)
+  val binding = Http().bindAndHandle(api, "localhost", 9071)
 
   StdIn.readLine()
 
