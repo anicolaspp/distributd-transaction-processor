1ae654ad7de86119197afce2f62916d1f47bb6a3
Nicolas A Perez <anicolaspp@gmail.com>
2016-07-06 00:19:55 -0400

    Refactoring tests

--- src/test/scala/AccountManagerSpec.scala
+++ src/test/scala/AccountManagerSpec.scala
@@ -52,45 +52,6 @@ class AccountManagerSpec extends FlatSpec with Matchers {
   }
 }
 
-class InMemoryAccountStorageSpec extends FlatSpec with Matchers {
-  it should "init with a balance" in {
-    val storage = InMemoryAccountStorage().storage
 
-    storage.get("101") should be (Account("101", 0))
-  }
-
-  it should "update" in {
-    val storage = InMemoryAccountStorage().storage
-
-    val account = storage.update(Account("101", 100))
-
-    account.id should be ("101")
-    account.balance should be (100)
-
-    storage.get("101") should be (account)
-  }
-
-  it should "manage multi accounts" in {
-    val storage = InMemoryAccountStorage().storage
-
-    (1 to 100) map { i =>
-      if (i % 2 == 0) {
-        storage.update(Account(i.toString, 100))
-      }
-      else {
-        storage.update(Account(i.toString, 200))
-      }
-    }
-
-    (1 to 100) foreach { i =>
-      if (i % 2 == 0) {
-        storage.get(i.toString).balance should be (100)
-      }
-      else {
-        storage.get(i.toString).balance should be (200)
-      }
-    }
-  }
-}
 
 
---
+++ src/test/scala/InMemoryAccountStorageSpec.scala
@@ -0,0 +1,46 @@
+/**
+  * Created by anicolaspp on 7/6/16.
+  */
+import com.nico.persistence.{Account, InMemoryAccountStorage}
+import org.scalatest.{Matchers, FlatSpec}
+
+class InMemoryAccountStorageSpec extends FlatSpec with Matchers {
+  it should "init with a balance" in {
+    val storage = InMemoryAccountStorage().storage
+
+    storage.get("101") should be (Account("101", 0))
+  }
+
+  it should "update" in {
+    val storage = InMemoryAccountStorage().storage
+
+    val account = storage.update(Account("101", 100))
+
+    account.id should be ("101")
+    account.balance should be (100)
+
+    storage.get("101") should be (account)
+  }
+
+  it should "manage multi accounts" in {
+    val storage = InMemoryAccountStorage().storage
+
+    (1 to 100) map { i =>
+      if (i % 2 == 0) {
+        storage.update(Account(i.toString, 100))
+      }
+      else {
+        storage.update(Account(i.toString, 200))
+      }
+    }
+
+    (1 to 100) foreach { i =>
+      if (i % 2 == 0) {
+        storage.get(i.toString).balance should be (100)
+      }
+      else {
+        storage.get(i.toString).balance should be (200)
+      }
+    }
+  }
+}
