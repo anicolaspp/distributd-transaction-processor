4fbe3d7d6d665117ae95b1d0e779a22f80ae382c
Nicolas A Perez <anicolaspp@gmail.com>
2016-07-10 10:35:30 -0400

    Exploring Distributed Data

---
+++ actors/src/main/scala/simulation/actors/cluster/replicator/DistributedStorage.scala
@@ -0,0 +1,24 @@
+/**
+  * Created by anicolaspp on 7/9/16.
+  */
+
+package simulation.actors.cluster.replicator
+
+import akka.actor.{Actor, ActorLogging}
+import akka.cluster.ddata.{DistributedData, ORSet, ORSetKey, Replicator}
+import akka.cluster.ddata.Replicator._
+import scala.concurrent.duration._
+
+class DistributedStorage extends Actor with ActorLogging {
+
+
+  val replicator = DistributedData(context.system).replicator
+
+  val key = ORSetKey[String]("key")
+
+  override def receive: Receive = {
+    case _  =>  {
+      replicator ! Update(key, ORSet.empty[String], WriteMajority(5.seconds))(_ + "hello")
+    }
+  }
+}
